c12e2bf5833197df77f0e3e77a1b61380cab2a8c
djowens <djowens826@gmail.com>
2016-03-18 11:56:00 -0700

    Adding my Atom config so I can share that across all other things -djo

---
+++ atom/packages/atom-autocomplete-php/php/tmp.php
@@ -0,0 +1,1 @@
+<?php $config = array( 'composer' => '/usr/local/bin/composer', 'php' => '/usr/bin/php', 'autoload' => array('vendor/autoload.php',), 'classmap' => array('vendor/composer/autoload_classmap.php','autoload/ezp_kernel.php',) );
---
+++ atom/packages/nuclide/pkg/nuclide/datatip-interfaces/lib/types.js
@@ -0,0 +1,15 @@
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+// A unique name for the provider to be used for analytics.
+// It is recommended that it be the name of the provider's package.
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJ0eXBlcy5qcyIsInNvdXJjZXNDb250ZW50IjpbXX0=
